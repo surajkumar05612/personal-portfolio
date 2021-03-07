b115a2de0dc166574dd5ddd0d7113c4b0178081b
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2021-03-07 23:07:08 +0530

    initiated

---
+++ script.js
@@ -0,0 +1,18 @@
+/*navbar script*/
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky")
+        }else{
+            $('.navbar').removeClass("sticky")
+        }
+    });
+
+/*navbar toggling*/
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+});
