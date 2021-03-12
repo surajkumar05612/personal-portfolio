b9ccfbedb3f2b2dd04d03e42371f2e3313437976
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2021-03-12 22:34:42 +0530

    updated
    fixed issues with contact form
    linked firebase to form

--- script.js
+++ script.js
@@ -66,5 +66,57 @@ var typed = new Typed(".typing-1",{
             }
         }
     });
-
 });
+
+//firebase
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDf6Ow9DVoMLDArsjDBNtVN9wnggg4mzyU",
+    authDomain: "portfolio-contact-data.firebaseapp.com",
+    databaseURL: "https://portfolio-contact-data-default-rtdb.firebaseio.com",
+    projectId: "portfolio-contact-data",
+    storageBucket: "portfolio-contact-data.appspot.com",
+    messagingSenderId: "240241685152",
+    appId: "1:240241685152:web:0d39584a154bb624b60fc5",
+    measurementId: "G-24S1LCNKFS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactform').addEventListener('submit', submitform);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var subject = getInputVal('subject');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
